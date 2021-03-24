e8b8f1a7021c32dabe1c4b91cf3312f79f8a0fa5
Sam Driver <sdriver@gmx.co.uk>
2021-03-24 16:11:33 +0000

    disable registering new users

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+// use all the Auth routes, but disable registering new users
+// Auth::routes();
+Auth::routes(['register' => false]);
+
