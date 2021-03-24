185ce60a5c5999e78f439036660d722bce8af29c
Sam Driver <sdriver@gmx.co.uk>
2021-03-24 20:47:59 +0000

    add employee routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'HomeController@index')->name('home');
 
+Route::get('/employee/all', 'EmployeeController@index')->name('employee.all');
+Route::get('/employee/{id}', 'EmployeeController@show')->name('employee.show');
+Route::get('/employee/{id}/edit', 'EmployeeController@edit')->name('employee.edit');
+Route::delete('/employee/{id}', 'EmployeeController@destroy')->name('employee.destroy');
+Route::put('/employee/{id}', 'EmployeeController@create')->name('employee.create');
+
 // use all the Auth routes, but disable registering new users
 // Auth::routes();
 Auth::routes(['register' => false]);
