5baefb0043319af4db598dc24cee6b0808eb53c9
Sam Driver <sdriver@gmx.co.uk>
2021-03-24 20:45:56 +0000

    add employee list vidw

---
+++ resources/views/employee/list.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<div class="row justify-content-center">
+<div class="col-md-8">
+<div class="card">
+<div class="card-header">{{ __('employees.listTitle') }}</div>
+<div class="card-body">
+    <table class="table table-striped">
+    @foreach ($employees as $employee)
+        <tr>
+            <td class="name">
+                {{ $employee->first_name }} {{ $employee->last_name }}
+            </td>
+            <td class="show">
+                <a class="btn btn-link" href="{{ route('employee.show', $employee->id) }}">
+                    {{ __('employees.show') }}
+                </a>
+            </td>
+            <td class="edit">
+                <a class="btn btn-link" href="{{ route('employee.edit', $employee->id) }}">
+                    {{ __('employees.edit') }}
+                </a>
+            </td>
+            <td class="delete">
+                <form method="POST" action="{{ route('employee.destroy', $employee->id) }}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-link">
+                        {{ __('employees.destroy') }}
+                    </button>
+                </form>
+            </td>
+        </tr>
+    @endforeach
+    </table>
+</div>
+</div>
+</div>
+</div>
+</div>
+@endsection
