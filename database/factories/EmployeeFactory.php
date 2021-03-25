07b4eb3083959cec4dcd57b54256adf4bec67e34
Sam Driver <sdriver@gmx.co.uk>
2021-03-25 09:32:52 +0000

    generate some employees with no company

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -5,8 +5,14 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Employee::class, function (Faker $faker) {
     return [
-        // assign to a random company, or create a new one if none exist
         'company_id' => function() {
+            // some employees will have no company
+            $hasCompany = rand(0, 5) >= 1;
+            if (!$hasCompany) {
+                return null;
+            }
+            
+            // assign to a random company, or create a new one if none exist
             $company = Company::all()->random() ?? factory(Company::class)->create();
             return $company->id;
         },
