cdd45c7ec1ff27ab4a98dacd35ff524059e14325
Sam Driver <sdriver@gmx.co.uk>
2021-03-25 09:33:02 +0000

    improve company name generation

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -1,10 +1,11 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(App\Company::class, function (Faker $faker) {
     return [
-        'name' => $faker->word,
+        'name' => Str::ucfirst($faker->word) . ' ' . Str::ucfirst($faker->word),
         'email' => $faker->email,
         'logo' => ($faker->word) . '.png',
         'website' => $faker->url,
