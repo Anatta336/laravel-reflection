d1e0ed5810e95ec4e4f9ef13149760bd094486e4
Sam Driver <sdriver@gmx.co.uk>
2021-03-26 11:04:41 +0000

    minor formatting

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -6,23 +6,23 @@
 @section('companyContent')
 <p>Name: <strong>{{ $company->name }}</strong></p>
 <p>Email address:
-@if ($company->email)
-    <strong><a href="mailto:{{ $company->email }}">
-        {{ $company->email }}
-    </a></strong>
-@else
-    <strong>None</strong>
-@endif
+    @if ($company->email)
+        <strong><a href="mailto:{{ $company->email }}">
+            {{ $company->email }}
+        </a></strong>
+    @else
+        <strong>None</strong>
+    @endif
 </p>
 
 <p>Website:
-@if ($company->website)
-    <strong><a href="{{ $company->website }}">
-        {{ $company->website }}
-    </a></strong>
-@else
-    <strong>None</strong>
-@endif
+    @if ($company->website)
+        <strong><a href="{{ $company->website }}">
+            {{ $company->website }}
+        </a></strong>
+    @else
+        <strong>None</strong>
+    @endif
 </p>
 
 <p>Logo: <strong>notYetImplemented.jpg</strong></p>
