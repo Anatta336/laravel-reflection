3244e2e9d0cd4f32eb25185e40e377d3cd40a47d
Sam Driver <sdriver@gmx.co.uk>
2021-03-26 10:19:07 +0000

    hide create link while already creating

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.employee', [
     'header' => __('employees.createTitle'),
+    'hideCreateButton' => true, // user might think this "create" link will use current form values
 ])
 
 @section('employeeContent')
