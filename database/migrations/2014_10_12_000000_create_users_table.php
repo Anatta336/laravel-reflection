0c6f749d7c39c124d74cdc04223009d1bcb3d244
Sam Driver <sdriver@gmx.co.uk>
2021-03-26 14:54:39 +0000

    add basic permissions to user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,12 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('canEditCompany');
+            $table->boolean('canDeleteCompany');
+            $table->boolean('canCreateCompany');
+            $table->boolean('canEditEmployee');
+            $table->boolean('canDeleteEmployee');
+            $table->boolean('canCreateEmployee');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,11 +12,43 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // create a single admin user
+        // create an admin user with full rights
         $admin = new User();
         $admin->name = 'Administrator';
         $admin->email = 'admin@example.com';
         $admin->password = password_hash('password', PASSWORD_DEFAULT);
+        $admin->canEditCompany = true;
+        $admin->canDeleteCompany = true;
+        $admin->canCreateCompany = true;
+        $admin->canEditEmployee = true;
+        $admin->canDeleteEmployee = true;
+        $admin->canCreateEmployee = true;
         $admin->save();
+
+        // create a user with rights over employees, not companies
+        $manager = new User();
+        $manager->name = 'Manager';
+        $manager->email = 'manager@example.com';
+        $manager->password = password_hash('password', PASSWORD_DEFAULT);
+        $manager->canEditCompany = false;
+        $manager->canDeleteCompany = false;
+        $manager->canCreateCompany = false;
+        $manager->canEditEmployee = true;
+        $manager->canDeleteEmployee = true;
+        $manager->canCreateEmployee = true;
+        $manager->save();
+
+        // create another user with no special rights
+        $accountant = new User();
+        $accountant->name = 'Accountant';
+        $accountant->email = 'accountant@example.com';
+        $accountant->password = password_hash('password', PASSWORD_DEFAULT);
+        $accountant->canEditCompany = false;
+        $accountant->canDeleteCompany = false;
+        $accountant->canCreateCompany = false;
+        $accountant->canEditEmployee = false;
+        $accountant->canDeleteEmployee = false;
+        $accountant->canCreateEmployee = false;
+        $accountant->save();
     }
 }
