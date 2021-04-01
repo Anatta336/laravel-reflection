68f541f15a5f2545cae1365a1c31fa1a605dd7df
Sam Driver <sdriver@gmx.co.uk>
2021-04-01 09:53:22 +0100

    fix factory using hidden files as logo

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -6,13 +6,23 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 $factory->define(Company::class, function (Faker $faker) {
-    // pick a random logo image to use
-    $logos = Storage::files('public/logos');
-    $logo  = $logos[rand(0, count($logos) - 1)];
+    // find all available logo images
+    $logos = array_filter(
+        Storage::disk('public')->files('logos'),
+        function ($path) {
+            // exclude any file with a leading . such as .gitignore
+            return !Str::contains($path, '/.');
+        }
+    );
 
-    // get the image path into the right form
-    $logo = Str::after($logo, 'public/');
-    
+    if (count($logos) > 0) {
+        // pick a random logo to use
+        $logo = $logos[rand(0, count($logos) - 1)];
+    } else {
+        // no available logo images
+        $logo = '';
+    }
+
     return [
         'name'    => Str::ucfirst($faker->word) . ' ' . Str::ucfirst($faker->word),
         'email'   => $faker->email,
